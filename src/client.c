dbd11283c1b450a8b42573247db9024d31689c65
Emirhan Giray <emirhan.giray@std.yildiz.edu.tr>
2023-12-26 12:38:24 +0300

    added real time messaging

--- src/client.c
+++ src/client.c
@@ -5,14 +5,16 @@
 #include <arpa/inet.h>
 
 #define PORT 8081
+#define BUFFER_SIZE 1024
 
 int main()
 {
     int sock = 0;
     struct sockaddr_in server_addr;
+    char buffer[BUFFER_SIZE] = {0};
+    const char *message = "Hello from client";
 
-
-    //socket file descriptor
+    // socket file descriptor
     sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock < 0)
     {
@@ -24,19 +26,32 @@ int main()
     server_addr.sin_port = htons(PORT);
 
     // Convert IPv4 and IPv6 addresses from text to binary form
-    if (inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr) <= 0) {
+    if (inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr) <= 0)
+    {
         printf("\nInvalid address/ Address not supported \n");
         return -1;
     }
 
-    int connection = connect(sock, (struct sockaddr*)&server_addr, sizeof(server_addr));
+    int connection = connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr));
     if (connection < 0)
     {
         perror("Connection failed");
         exit(EXIT_FAILURE);
     }
 
+    while (1)
+    {
+        // Send message to server
+        send(sock, message, strlen(message), 0);
+        printf("Message sent to server\n");
 
+        // Receive message from server
+        int valread = recv(sock, buffer, BUFFER_SIZE, 0);
+        printf("Server: %s\n", buffer);
+
+        // Clear the buffer
+        memset(buffer, 0, sizeof(buffer));
+    }
 
     return 0;
 }
